36d20d2c8d9253319f759394cde9c824588296cc
Marc <marcdvlpr@gmail.com>
2024-07-18 13:40:15 +0200

    Init project

---
+++ HelloMedic/HelloMedic/HelloMedicApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloMedicApp.swift
+//  HelloMedic
+//
+//  Created by Apprenant 124 on 18/07/2024.
+//
+
+import SwiftUI
+
+@main
+struct HelloMedicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
