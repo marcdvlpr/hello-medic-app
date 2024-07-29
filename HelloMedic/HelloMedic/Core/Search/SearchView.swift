2922391e7785f44bd6eb6da4a5d7efa907dbca87
Kanita <kanitalim88@gmail.com>
2024-07-29 09:34:42 +0200

    splashScreea

---
+++ HelloMedic/HelloMedic/Core/Search/SearchView.swift
@@ -0,0 +1,18 @@
+//
+//  SearchView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 172 on 19/07/2024.
+//
+
+import SwiftUI
+
+struct SearchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SearchView()
+}
