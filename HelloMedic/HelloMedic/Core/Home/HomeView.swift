3bce87affe30faef69bb376e912affd521e46135
Kanita <kanitalim88@gmail.com>
2024-07-29 09:34:42 +0200

    splashScreea

---
+++ HelloMedic/HelloMedic/Core/Home/HomeView.swift
@@ -0,0 +1,20 @@
+//
+//  HomeView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 172 on 19/07/2024.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    var body: some View {
+        
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomeView()
+}
---
+++ HelloMedic/HelloMedic/Core/Splash/SplashView.swift
@@ -0,0 +1,45 @@
+//
+//  SplashView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 172 on 19/07/2024.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    
+    @State private var scale = 0.7
+    @Binding var isActive: Bool
+    
+    var body: some View {
+        
+        VStack {
+            Text("HelloDoc")
+                .font(.custom("Baloo", fixedSize: 60))
+                .fontWeight(.regular)
+                .foregroundColor(Color("hmBlue"))
+            Image("hmLogo")
+                .resizable()
+                .frame(width: 280, height: 280)
+                .scaledToFit()
+                .scaleEffect(scale)
+                .onAppear{
+                    withAnimation(.easeIn(duration: 2)) {
+                        self.scale = 1.2
+                    }
+                }
+        }.onAppear {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3.5) {
+                withAnimation {
+                    self.isActive = true
+                }
+            }
+        }
+        
+    }
+}
+
+#Preview {
+    SplashView(isActive: .constant(false))
+}
