559f304d0d078e7fa9caf8c81fc894375b62b04f
Anne-Lise <annelisecoco.21@gmail.com>
2024-07-30 16:38:25 +0200

    first commit RegisterViewone and RegisterViewtwo

---
+++ HelloMedic/HelloMedic/Core/Register/RegisterViewOne.swift
@@ -0,0 +1,82 @@
+//
+//  RegisterViewOne.swift
+//  HelloMedic
+//
+//  Created by Apprenant 162 on 29/07/2024.
+//
+
+import Foundation
+import UIKit
+
+class RegisterViewOne: UIViewController{
+    
+    let enteremail = UITextField()
+    let enterpassword = UITextField()
+    let loginButton = UIButton(type: .system)
+    let errorlabel = UILabel()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    func connexionUI(){
+        
+        enteremail.placeholder = "E-mail"
+        enteremail.borderStyle = .roundedRect
+        enteremail.keyboardType = .emailAddress
+        enteremail.autocapitalizationType = .none
+        enteremail.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(enteremail)
+        
+        enterpassword.placeholder = "Mot de passe"
+        enteremail.borderStyle = .roundedRect
+        enterpassword.isSecureTextEntry = true
+        enterpassword.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(enterpassword)
+        
+        loginButton.setTitle("Se connecter", for: .normal)
+        loginButton.addTarget(self, action: #selector (handleLogin), for: .touchUpInside)
+        loginButton.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview(loginButton)
+        errorlabel.textColor = .red
+        errorlabel.numberOfLines = 0
+        errorlabel.textAlignment = .center
+        errorlabel.translatesAutoresizingMaskIntoConstraints = false
+                view.addSubview(errorlabel)
+        
+        NSLayoutConstraint.activate(
+            [enteremail.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 50),
+            enteremail.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            enteremail.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+             
+            enterpassword.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            enterpassword.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
+            enterpassword.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            loginButton.topAnchor.constraint(equalTo:loginButton.bottomAnchor, constant: 20),
+            loginButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+             
+            errorlabel.topAnchor.constraint(equalTo: loginButton.bottomAnchor, constant: 20),
+            errorlabel.leadingAnchor.constraint(equalTo:view.leadingAnchor, constant: 20),
+            errorlabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20)
+            ])
+                
+    }
+    @objc func handleLogin(){
+        guard let email = enteremail.text, isValidEmail(email) else {
+            showError("Veuillez entrer une adresse email valide.")
+            return
+        }
+        guard let password = enterpassword.text, !password.isEmpty else {
+            showError("Veuillez entrer votre mot de passe.")
+            return
+        }
+        errorlabel.text = ""
+    }
+    func isValidEmail(_ email: String) -> Bool {
+            let emailEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+            let emailPred = NSPredicate(format: "SELF MATCHES %@", emailEx)
+            return emailPred.evaluate(with: email)
+        }
+    func showError(_ message: String) {
+            errorlabel.text = message
+        }
+    }
---
+++ HelloMedic/HelloMedic/Core/Register/RegisterViewTwo.swift
@@ -0,0 +1,104 @@
+//
+//  RegisterView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 162 on 29/07/2024.
+//
+
+import SwiftUI
+
+/// Structure RegisterView pour s'enregistrer lorsqu'on arrive sur l'application
+struct RegisterViewTwo: View {
+    /// Variable d'état pour le prénom
+    @State private var firstname: String = ""
+    /// Variable d'état pour le nom
+    @State private var lastname: String = ""
+    /// Variable d'état pour  la date de naissance
+    @State private var dateofBirth: Date = Date()
+    /// Variable d'état pour l'adresse
+    @State private var address: String = ""
+    /// Variable d'état pour le code postale
+    @State private var zipcode: String = ""
+    /// Variable d'état pour la ville
+    @State private var city: String = ""
+    /// Variable d'état pour le genre
+    @State private var selectedgender: Int = 0
+    /// Variable d'état pour afficher la vue Popup
+    @State private var isShowingPopup: Bool = false
+   
+    /// Déclaration de la constante pour
+    let genders = ["Homme", "Femme", "Autre"]
+    
+    /// Variable pour vérifier si tous les champs obligatoires sont remplis et si la date de naissance est autre que celle du jour
+    var isFormValid: Bool {
+    !firstname.isEmpty && !lastname.isEmpty && !address.isEmpty && !zipcode.isEmpty && !city.isEmpty && Calendar.current.isDateInToday(dateofBirth) == false }
+    
+    var body: some View {
+        
+        ZStack{
+            Rectangle()
+                .frame(width: 400, height: 100)
+                .foregroundStyle(.hmBlue)
+            Text("S'enregistrer")
+                .font(.largeTitle)
+                .foregroundStyle(Color.white)
+        }
+        Form{
+            Section{
+             Text("Informations personnelles requises")
+                Picker("Sélectionnez votre genre", selection: $selectedgender) {
+                    ForEach(0..<3) { index in
+                        Text(self.genders[index]).tag(index).font(.title)
+                             }
+                }
+                TextField("Prénom", text: $firstname)
+                TextField("Nom", text: $lastname)
+                DatePicker("Date de Naissance", selection: $dateofBirth,displayedComponents: .date)
+                TextField("Adresse complète", text: $address)
+                TextField("Code Postal", text: $zipcode)
+                TextField("Ville", text: $city)
+            }
+        }
+            Button(action: {
+       /// Affiche la vue modale si tous les champs sont complétés
+                isShowingPopup = true
+                           }) {
+                Text("Soumettre")
+                    .padding()
+                           }.sheet(isPresented: $isShowingPopup) {
+                               PopupView(isShowingPopup: $isShowingPopup)
+                           }
+                           .disabled(!isFormValid)
+    }
+}
+struct PopupView: View {
+    
+    /// Variable qui détermine si la pop-up est affichée
+    @Binding var isShowingPopup: Bool
+    
+    var body: some View {
+        VStack {
+            Text("Vos informations on bien été enregistrées. Elles seront transmises à vos praticiens.")
+                .font(.largeTitle)
+                .padding()
+                .foregroundStyle(Color.white)
+            Button(action: {
+                isShowingPopup = false
+            }) {
+            Text("Fermer")
+                    .padding()
+                    .background(Color.hmBlue)
+                    .foregroundColor(.white)
+                    .cornerRadius(8)
+            }
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .background(Color.hmSkyBlue)
+        .cornerRadius(20)
+        .shadow(radius: 20)
+        .padding()
+    }
+}
+#Preview {
+    RegisterViewTwo()
+}
