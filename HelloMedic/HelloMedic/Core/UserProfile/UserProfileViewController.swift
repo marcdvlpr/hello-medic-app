bde7e2259c967e3514a3036b58defe0308080b7f
Marc <marcdvlpr@gmail.com>
2024-07-30 09:54:06 +0200

    Add imageProfile in UserProfileViewController

--- HelloMedic/HelloMedic/Core/UserProfile/UserProfileViewController.swift
+++ HelloMedic/HelloMedic/Core/UserProfile/UserProfileViewController.swift
@@ -8,7 +8,36 @@
 import SwiftUI
 
 class UserProfileViewController: UIViewController {
+
+    private let userProfileView: UIView = {
+        let view = UIView()
+        return view
+    }()
+    
+    private let imageProfile: UIImageView = {
+        let image = UIImageView()
+        image.image = UIImage(named: "hmLogo")
+        image.layer.borderWidth = 1
+        image.layer.borderColor = UIColor.black.cgColor
+        image.translatesAutoresizingMaskIntoConstraints = false
+        return image
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        userProfileView.addSubview(imageProfile)
+        
+        view.addSubview(userProfileView)
+        
+        setupAutoLayout()
+    }
+    
+    func setupAutoLayout() {
+        imageProfile.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
+        imageProfile.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        imageProfile.heightAnchor.constraint(equalToConstant: 100).isActive = true
+        imageProfile.widthAnchor.constraint(equalToConstant: 100).isActive = true
+        imageProfile.layer.cornerRadius = imageProfile.frame.width / 2
     }
 }
