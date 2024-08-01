b342ecc4e78b5b08ec0aec9aac07ebf01ecc98ea
Elie BINGUE <eliedunamis7@icloud.com>
2024-08-01 10:56:09 +0200

    feature/login-screen create component LogoViewComponent

---
+++ HelloMedic/HelloMedic/Core/Login/LoginView.swift
@@ -0,0 +1,18 @@
+//
+//  LoginView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 01/08/2024.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LoginView()
+}
