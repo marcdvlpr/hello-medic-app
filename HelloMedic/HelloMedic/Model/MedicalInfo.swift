fd2e75187cbbca52cb2c20c5d15f67f8595e5e13
Marc <marcdvlpr@gmail.com>
2024-08-02 11:42:46 +0200

    Add MedicalInfo model and update medicalInfo.json

---
+++ HelloMedic/HelloMedic/Model/MedicalInfo.swift
@@ -0,0 +1,18 @@
+//
+//  MedicalInfo.swift
+//  HelloMedic
+//
+//  Created by Apprenant 124 on 02/08/2024.
+//
+
+import Foundation
+
+struct MedicalInfo: Decodable {
+    let id: String
+    let userID: String
+    let bloodType: String
+    let allergies: [String]
+    let height: Int
+    let weight: Double
+    let wheelchair: Bool
+}
