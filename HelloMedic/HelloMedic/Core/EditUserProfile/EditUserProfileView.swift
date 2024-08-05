432059dc4af8734041b0fbc92999ca46c04612b4
Marc <marcdvlpr@gmail.com>
2024-08-05 16:44:45 +0200

    Add RowEditProfileView and use it in EditUserProfileView

--- HelloMedic/HelloMedic/Core/EditUserProfile/EditUserProfileView.swift
+++ HelloMedic/HelloMedic/Core/EditUserProfile/EditUserProfileView.swift
@@ -10,18 +10,36 @@ import SwiftUI
 struct EditProfileView: View {
     
     @State private var firstName: String = ""
+    @State private var lastName: String = ""
+    @State private var dateOfBirth: String = ""
+    @State private var gender: String = ""
+    @State private var address: String = ""
+    @State private var postalCode: String = ""
+    @State private var city: String = ""
+    @State private var email: String = ""
+    @State private var password: String = ""
+    @State private var phone: String = ""
     
     var body: some View {
         VStack {
             List {
                 Section("Information Personnelle ") {
-                    HStack(spacing: 10) {
-                        Text("Prénoms")
-                        Spacer()
-                        TextField("John", text: $firstName)
-                            .textFieldStyle(.plain)
-                            .multilineTextAlignment(.trailing)
-                    }
+                    RowEditProfileView(title: "Prénom",
+                                       text: $firstName)
+                    RowEditProfileView(title: "Nom",
+                                       text: $lastName)
+                    RowEditProfileView(title: "Date de Naissance",
+                                       text: $dateOfBirth)
+                    RowEditProfileView(title: "Genre",
+                                       text: $gender)
+                    RowEditProfileView(title: "Adresse",
+                                       text: $address)
+                    RowEditProfileView(title: "Code Postal",
+                                       text: $postalCode)
+                    RowEditProfileView(title: "Ville",
+                                       text: $city)
+                    RowEditProfileView(title: "Téléphone",
+                                       text: $phone)
                 }
             }
         }
