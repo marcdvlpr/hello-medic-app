0ed4d20013ad4d56973861d0f1da4a6986bfd2df
Marc <marcdvlpr@gmail.com>
2024-08-07 16:57:52 +0200

    Add AppointmentDetailsView and init AppointmentDetailsViewController

---
+++ HelloMedic/HelloMedic/Core/AppointmentDetails/AppointmentDetailsView.swift
@@ -0,0 +1,19 @@
+//
+//  AppointmentDetailsView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 124 on 07/08/2024.
+//
+
+import SwiftUI
+
+struct AppointmentDetailsView: UIViewControllerRepresentable {
+
+    func makeUIViewController(context: Context) -> some AppointmentDetailsViewController {
+        return AppointmentDetailsViewController()
+    }
+
+    func updateUIViewController(_ uiViewController: UIViewControllerType, context: Context) {
+        //
+    }
+}
---
+++ HelloMedic/HelloMedic/Core/AppointmentDetails/AppointmentDetailsViewController.swift
@@ -0,0 +1,14 @@
+//
+//  AppointmentDetailsViewController.swift
+//  HelloMedic
+//
+//  Created by Apprenant 124 on 07/08/2024.
+//
+
+import UIKit
+
+class AppointmentDetailsViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
