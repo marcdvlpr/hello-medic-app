93ed9b7ea683154e251b342607d1d7a1e66adb9b
Marc <marcdvlpr@gmail.com>
2024-08-08 11:15:01 +0200

    Add nameLabel and specialityLabel to AppointmentDetailsViewController

--- HelloMedic/HelloMedic/Core/AppointmentDetails/AppointmentDetailsViewController.swift
+++ HelloMedic/HelloMedic/Core/AppointmentDetails/AppointmentDetailsViewController.swift
@@ -12,6 +12,8 @@ class AppointmentDetailsViewController: UIViewController {
         super.viewDidLoad()
         
         appointmentDetailsView.addSubview(imageView)
+        appointmentDetailsView.addSubview(nameLabel)
+        appointmentDetailsView.addSubview(specialityLabel)
         
         view.addSubview(appointmentDetailsView)
         
@@ -26,11 +28,25 @@ class AppointmentDetailsViewController: UIViewController {
     
     private let imageView: UIImageView = {
         let image = UIImageView()
-        image.image = UIImage(named: "hmLogo")
-        image.layer.borderWidth = 1
-        image.layer.borderColor = UIColor.black.cgColor
+        image.image = UIImage(named: "drisabelle")
         image.translatesAutoresizingMaskIntoConstraints = false
         return image
+    }()
+    
+    private let nameLabel: UILabel = {
+        let text = UILabel()
+        text.text = "Dr Sam Smith"
+        text.textColor = UIColor(named: "hmBlue")
+        text.font = UIFont.systemFont(ofSize: 14, weight: .bold)
+        return text
+    }()
+    
+    private let specialityLabel: UILabel = {
+        let text = UILabel()
+        text.text = "Médecin Généraliste"
+        text.textColor = UIColor(named: "hmBlue")
+        text.font = UIFont.systemFont(ofSize: 12, weight: .light)
+        return text
     }()
     
     func setupAutoLayout() {
@@ -39,5 +55,19 @@ class AppointmentDetailsViewController: UIViewController {
         imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.layer.cornerRadius = imageView.frame.width / 2
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.heightAnchor.constraint(equalToConstant: 16).isActive = true
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 10).isActive = true
+        nameLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+        nameLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
+        nameLabel.textAlignment = .center
+        
+        specialityLabel.translatesAutoresizingMaskIntoConstraints = false
+        specialityLabel.heightAnchor.constraint(equalToConstant: 16).isActive = true
+        specialityLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 5).isActive = true
+        specialityLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+        specialityLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
+        specialityLabel.textAlignment = .center
     }
 }
