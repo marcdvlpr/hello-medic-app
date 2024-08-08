f9905841f7333fe76d759dfe1a827431ab1f0d9c
Marc <marcdvlpr@gmail.com>
2024-08-08 15:14:30 +0200

    Add estimatedTimeLabel to AppointmentDetailsViewController

--- HelloMedic/HelloMedic/Core/AppointmentDetails/AppointmentDetailsViewController.swift
+++ HelloMedic/HelloMedic/Core/AppointmentDetails/AppointmentDetailsViewController.swift
@@ -14,6 +14,7 @@ class AppointmentDetailsViewController: UIViewController {
         appointmentDetailsView.addSubview(imageView)
         appointmentDetailsView.addSubview(nameLabel)
         appointmentDetailsView.addSubview(specialityLabel)
+        appointmentDetailsView.addSubview(estimatedTimeLabel)
         
         view.addSubview(appointmentDetailsView)
         
@@ -22,20 +23,21 @@ class AppointmentDetailsViewController: UIViewController {
     
     private let appointmentDetailsView: UIView = {
         let view = UIView()
-        view.backgroundColor = .red
         return view
     }()
     
     private let imageView: UIImageView = {
         let image = UIImageView()
         image.image = UIImage(named: "drisabelle")
-        image.translatesAutoresizingMaskIntoConstraints = false
+        image.clipsToBounds = true
+        image.layer.cornerRadius = 50
+        image.contentMode = .scaleAspectFill
         return image
     }()
     
     private let nameLabel: UILabel = {
         let text = UILabel()
-        text.text = "Dr Sam Smith"
+        text.text = "Dr Lisa Smith"
         text.textColor = UIColor(named: "hmBlue")
         text.font = UIFont.systemFont(ofSize: 14, weight: .bold)
         return text
@@ -49,12 +51,24 @@ class AppointmentDetailsViewController: UIViewController {
         return text
     }()
     
+    private let estimatedTimeLabel: UILabel = {
+        let text = UILabel()
+        text.text = "Arrivée estimée 14:30"
+        text.backgroundColor = UIColor(.white)
+        text.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        text.layer.borderWidth = 1
+        text.layer.borderColor = UIColor(named: "hmBlue")?.cgColor
+        text.clipsToBounds = true
+        text.layer.cornerRadius = 10
+        return text
+    }()
+    
     func setupAutoLayout() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 30).isActive = true
         imageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         imageView.heightAnchor.constraint(equalToConstant: 100).isActive = true
         imageView.widthAnchor.constraint(equalToConstant: 100).isActive = true
-        imageView.layer.cornerRadius = imageView.frame.width / 2
         
         nameLabel.translatesAutoresizingMaskIntoConstraints = false
         nameLabel.heightAnchor.constraint(equalToConstant: 16).isActive = true
@@ -69,5 +83,13 @@ class AppointmentDetailsViewController: UIViewController {
         specialityLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
         specialityLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
         specialityLabel.textAlignment = .center
+        
+        estimatedTimeLabel.translatesAutoresizingMaskIntoConstraints = false
+        estimatedTimeLabel.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        estimatedTimeLabel.topAnchor.constraint(equalTo: specialityLabel.bottomAnchor, constant: 10).isActive = true
+        estimatedTimeLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        estimatedTimeLabel.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 16).isActive = true
+        estimatedTimeLabel.trailingAnchor.constraint(equalTo: self.view.trailingAnchor, constant: -16).isActive = true
+        estimatedTimeLabel.textAlignment = .center
     }
 }
