f2c429a7987ea0a8f38958ec4c901e0a4ab9e3ec
Elie BINGUE <eliedunamis7@icloud.com>
2024-08-18 00:19:51 +0200

    feature/rendez-vous Add CareGIver file

---
+++ HelloMedic/HelloMedic/Core/FavorisView/CareGiver.swift
@@ -0,0 +1,17 @@
+//
+//  CareGiver1.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 17/08/2024.
+//
+
+import Foundation
+
+struct CareGiver: Identifiable {
+    let id = UUID()
+    let name: String
+    let specialty: String
+    let image: String
+    let specialtyImage: String
+}
+
---
+++ HelloMedic/HelloMedic/Core/FavorisView/FavoriteCareGiversData.swift
@@ -0,0 +1,43 @@
+//
+//  FavoriteCareGiversData1.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 17/08/2024.
+//
+
+import Foundation
+
+struct FavoriteCareGiversData1 {
+   static let careGivers: [CareGiver] = [
+      CareGiver(
+         name: "Dr Mickael Stevensen",
+         specialty: "Généraliste",
+         image: "dralexandre",
+         specialtyImage: "stethoscope"
+      ),
+      CareGiver(
+         name: "Dr Sam Robertson",
+         specialty: "Pédiatre",
+         image: "drfrancois",
+         specialtyImage: "figure.and.child.holdinghands"
+      ),
+      CareGiver(
+         name: "Ms jennyfer Fergusson",
+         specialty: "Infirmière",
+         image: "drlisa",
+         specialtyImage: "cross.case"
+      ),
+      CareGiver(
+         name: "Ms johanna Smith",
+         specialty: "Kiné",
+         image: "drmeredith",
+         specialtyImage: "figure.walk"
+      ),
+      CareGiver(
+         name: "Ms Deborah Benett",
+         specialty: "Aide-soignante",
+         image: "drisabelle",
+         specialtyImage: "bandage"
+      )
+   ]
+}
