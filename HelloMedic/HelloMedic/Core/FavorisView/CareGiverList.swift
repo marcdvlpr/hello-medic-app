cb7a89dfdc2856ceedabd074ac64d23dc438df00
Elie BINGUE <eliedunamis7@icloud.com>
2024-08-18 00:19:51 +0200

    feature/rendez-vous Add CareGIver file

---
+++ HelloMedic/HelloMedic/Core/FavorisView/CareGiverList.swift
@@ -0,0 +1,35 @@
+//
+//  CareGiverList1.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 17/08/2024.
+//
+
+import SwiftUI
+
+struct CaregiverList: View {
+   let careGivers: [CareGiver]
+
+   var body: some View {
+      List {
+         ForEach(careGivers) { caregiver in
+            HStack {
+               Image(caregiver.image)
+                  .resizable()
+                  .aspectRatio(contentMode: .fit)
+                  .frame(width: 50, height: 50)
+                  .clipShape(Circle())
+               VStack(alignment: .leading) {
+                  Text(caregiver.name)
+                     .font(.headline)
+                  Text(caregiver.specialty)
+                     .font(.subheadline)
+                     .foregroundStyle(.secondary)
+               }
+            }
+         }
+      }
+      .listStyle(PlainListStyle())
+   }
+}
+
---
+++ HelloMedic/HelloMedic/Core/FavorisView/MainView.swift
@@ -0,0 +1,53 @@
+//
+//  MainView1.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 17/08/2024.
+//
+
+import SwiftUI
+
+struct MainView: View {
+   @State private var searchText = ""
+   @State private var selectedSpecialty: String? = nil
+   @State private var favoriteCareGivers = FavoriteCareGiversData.careGivers
+
+   var filteredCareGivers: [CareGiver] {
+      favoriteCareGivers.filter { caregiver in
+         (selectedSpecialty == nil || caregiver.specialty == selectedSpecialty) &&
+         (searchText.isEmpty ||
+          caregiver.name.localizedCaseInsensitiveContains(searchText) ||
+          caregiver.specialty.localizedCaseInsensitiveContains(searchText))
+      }
+   }
+
+   var body: some View {
+      NavigationView {
+         VStack {
+            SpecialtyScrollView(
+               careGivers: favoriteCareGivers,
+               selectedSpecialty: $selectedSpecialty,
+               searchText: $searchText
+            )
+            Text("Favoris")
+               .font(.system(.title3, weight: .bold))
+               .frame(maxWidth: .infinity, alignment: .leading)
+               .padding(10)
+            CaregiverList(careGivers: filteredCareGivers)
+         }
+         .navigationTitle("Bonjour, Lisa")
+         .searchable(text: $searchText, prompt: "rechercher une spécialité")
+         .onChange(of: searchText) { oldValue, newValue in
+            selectedSpecialty = nil
+            print("Search Text: \(newValue)")
+         }
+      }
+   }
+}
+
+
+
+#Preview {
+   MainView()
+}
+
