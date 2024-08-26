8172fd75b60e7b9c46f8d7fcf7c571cc45c32f6f
Elie BINGUE <eliedunamis7@icloud.com>
2024-08-26 04:12:05 +0200

    feature/rendez-vous Add RendezVousView file

---
+++ HelloMedic/HelloMedic/Core/RendezVous/Components/DateSelectionView.swift
@@ -0,0 +1,18 @@
+//
+//  DateSelectionView.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 25/08/2024.
+//
+
+import SwiftUI
+
+struct DateSelectionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DateSelectionView()
+}
