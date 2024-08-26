663b2fbdb05c2b02e940dc9829569d9940a95b35
Elie BINGUE <eliedunamis7@icloud.com>
2024-08-26 05:32:49 +0200

    feature/rendez-vous Add PractitionerViewControlle file

--- HelloMedic/HelloMedic/Core/RendezVous/Components/PractitionerCardViewViewController.swift
+++ HelloMedic/HelloMedic/Core/RendezVous/Components/PractitionerCardViewViewController.swift
@@ -7,23 +7,24 @@
 
 import UIKit
 
-class PractitionerCardViewViewController: UIViewController {
+class PractitionerViewController: UIViewController {
+   override func viewDidLoad() {
+      super.viewDidLoad()
+      view.backgroundColor = .white
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+      let cardView = PractitionerCardView()
+      cardView.translatesAutoresizingMaskIntoConstraints = false
+      view.addSubview(cardView)
 
-        // Do any additional setup after loading the view.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+         // Contraintes pour la carte
+      NSLayoutConstraint.activate([
+         cardView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 20),
+         cardView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+         //             cardView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+         cardView.widthAnchor.constraint(equalToConstant: 350),
+         cardView.heightAnchor.constraint(equalToConstant: 170)
+      ])
+   }
 }
+
+
