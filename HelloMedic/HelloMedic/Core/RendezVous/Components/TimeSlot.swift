9da834cf6f6f8dae0172cc88d091f01d9f8e5785
Elie BINGUE <eliedunamis7@icloud.com>
2024-08-26 04:13:53 +0200

    feature/rendez-vous Add TimeSlot file

--- HelloMedic/HelloMedic/Core/RendezVous/Components/TimeSlot.swift
+++ HelloMedic/HelloMedic/Core/RendezVous/Components/TimeSlot.swift
@@ -5,14 +5,10 @@
 //  Created by Apprenant 125 on 25/08/2024.
 //
 
-import SwiftUI
+import Foundation
 
-struct TimeSlot: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
-}
-
-#Preview {
-    TimeSlot()
+struct TimeSlot: Identifiable {
+   let id = UUID()
+   let time: String
+   let isAvailable: Bool
 }
