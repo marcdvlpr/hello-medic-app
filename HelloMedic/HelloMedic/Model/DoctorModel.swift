f2fc11da8d340c4f63484b0f5031420300f17149
Kanita <kanitalim88@gmail.com>
2024-09-12 15:16:26 +0200

    change var to let

--- HelloMedic/HelloMedic/Model/DoctorModel.swift
+++ HelloMedic/HelloMedic/Model/DoctorModel.swift
@@ -12,24 +12,24 @@ import Foundation
 /// `MedicalCertification` inclut des informations sur l'école où le médecin a obtenu son diplôme
 /// ainsi que le type de diplôme reçu.
 struct MedicalCertification: Codable {
-    var school: String /// L'école où le médecin a obtenu son diplôme.
-    var diplome: String /// Le type de diplôme obtenu par le médecin.
+    let school: String /// L'école où le médecin a obtenu son diplôme.
+    let diplome: String /// Le type de diplôme obtenu par le médecin.
 }
 
 /// Représente un médecin avec ses détails.
 struct Doctor: Identifiable, Codable {
     let id: String /// Identifiant unique pour chaque docteur.
-    var pict: String /// Nom de l'image du docteur.
-    var name: String /// Nom du docteur.
-    var specialty: String /// Spécialité du docteur.
-    var rating: Int /// Évaluation du docteur.
-    var distance: String /// Distance jusqu'au docteur.
-    var availability: String /// Les horaires de disponibilité du médecin.
-    var perimeter : String /// Le périmètre dans lequel le médecin intervient.
-    var phoneNumber: String /// Le numéro de téléphone du médecin.
-    var paymentMethod: String /// Les méthodes de paiement acceptées par le médecin.
-    var languages: String /// Les langues parlées par le médecin.
-    var horaires: String /// Les horaires spécifiques ou supplémentaires du médecin.
-    var certification : MedicalCertification /// La certification médicale du médecin.
+    let pict: String /// Nom de l'image du docteur.
+    let name: String /// Nom du docteur.
+    let specialty: String /// Spécialité du docteur.
+    let rating: Int /// Évaluation du docteur.
+    let distance: String /// Distance jusqu'au docteur.
+    let availability: String /// Les horaires de disponibilité du médecin.
+    let perimeter : String /// Le périmètre dans lequel le médecin intervient.
+    let phoneNumber: String /// Le numéro de téléphone du médecin.
+    let paymentMethod: String /// Les méthodes de paiement acceptées par le médecin.
+    let languages: String /// Les langues parlées par le médecin.
+    let horaires: String /// Les horaires spécifiques ou supplémentaires du médecin.
+    let certification : MedicalCertification /// La certification médicale du médecin.
     
 }
