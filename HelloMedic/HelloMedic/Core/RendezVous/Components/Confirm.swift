52b428f77cf089daa489879cc4e4aa0ed6317729
Elie BINGUE <eliedunamis7@icloud.com>
2024-09-14 17:01:58 +0200

    Add a Sheet View for confirmed appointment

---
+++ HelloMedic/HelloMedic/Core/RendezVous/Components/Confirm.swift
@@ -0,0 +1,31 @@
+//
+//  Confirm.swift
+//  HelloMedic
+//
+//  Created by Apprenant 125 on 14/09/2024.
+//
+
+import SwiftUI
+
+struct Confirm: View {
+   @State private var isShowingSheet = false
+
+   var body: some View {
+      VStack {
+         Text("Merci !")
+            .font(.title)
+            .padding(50)
+         Text("""
+                  Votre rendez-vous 
+                     est confirmé !
+               """)
+         .multilineTextAlignment(.center)
+         .font(.title)
+      }
+      .presentationDetents([.height(250)])
+   }
+}
+
+#Preview {
+   Confirm()
+}
--- HelloMedic/HelloMedic/Core/RendezVous/RendezVousView.swift
+++ HelloMedic/HelloMedic/Core/RendezVous/RendezVousView.swift
@@ -5,7 +5,6 @@
 //  Created by Apprenant 125 on 24/08/2024.
 //
 
-
 import SwiftUI
 
 struct RendezVousView2: View {
@@ -23,23 +22,39 @@ struct RendezVousView2: View {
    ]
 
    @State private var selectedTime: String? = nil
+   @State private var isShowingSheet = false
 
    var body: some View {
-         VStack {
+      VStack {
+         PractitionerViewControllerRepresentable()
+         Spacer()
 
-            PractitionerViewControllerRepresentable()
-            Spacer()
+         DateSelectionView(daysOfWeek: daysOfWeek, dates: dates, isDaySelected: isDaySelected)
+            .padding(.bottom, 60)
 
-            DateSelectionView(daysOfWeek: daysOfWeek, dates: dates, isDaySelected: isDaySelected)
-               .padding(.bottom, 60)
+         TimeSlotSelectionView(timeSlots: timeSlots, selectedTime: $selectedTime)
+            .padding(.bottom, 60)
 
-            TimeSlotSelectionView(timeSlots: timeSlots, selectedTime: $selectedTime)
-               .padding(.bottom, 60)
-
-            ConfirmationButton()
+         Button(action: {
+            isShowingSheet.toggle()
+         }) {
+            Text("Confirmer le rendez-vous")
+               .padding()
+               .frame(maxWidth: .infinity)
+               .background(Color.blue)
+               .foregroundColor(.white)
+               .cornerRadius(8)
+               .padding(20)
+         }
+         Spacer()
+         .sheet(isPresented: $isShowingSheet) {
+            Confirm()
+               .presentationDetents([.height(300)]) 
+               }
+            }
          }
       }
-   }
+   
 
 #Preview {
    RendezVousView2()
